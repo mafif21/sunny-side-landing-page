41fd3bbccb7b42b7879cb9383b565f4386abf1d4
Muhammad Afif <afif.maliki21@gmail.com>
2022-03-21 21:46:20 +0700

    make hamburger menu

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// hamburgerMenu.addEventListener("click", function () {
-//   console.log("ok");
-// });
-
 window.addEventListener("click", function (eve) {
   if (eve.target.className == "hamburger") {
     target = eve.target.nextElementSibling;
+    target2 = eve.target.previousElementSibling;
     target.classList.toggle("hamburger-screen");
+    target2.classList.toggle("triangle");
   }
 });
